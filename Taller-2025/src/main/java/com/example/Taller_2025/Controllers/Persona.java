e0d88ebd19085bbbf2d0681c584db1d1f95ebd89
Manuel <marcoschb27@gmail.com>
2025-11-06 11:25:18 -0500

    avance con el controller de persona y model

---
+++ Taller-2025/src/main/java/com/example/Taller_2025/Controllers/Persona.java
@@ -0,0 +1,33 @@
+package com.example.Taller_2025.Controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/persona")
+
+public class Persona {
+    @Autowired
+    PersonaService personaService;
+
+    @GetMapping("/listar")
+    public List<PersonaModel> listar(){
+        return personaService.listar();
+    }
+
+    @PutMapping("/editar")
+    public PersonaModel editar(@RequestBody PersonaModel persona){
+        return personaService.editar(persona);
+    }
+    @DeleteMapping("/eliminar")
+    public void eliminar(@RequestAttribute("id") int id){
+        personaService.eliminar(id);
+    }       
+
+}
---
+++ Taller-2025/src/main/java/com/example/Taller_2025/Models/PersonaModel.java
@@ -0,0 +1,64 @@
+import
+package com.example.Taller_2025.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "persona")
+public class PersonaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public PersonaModel() {
+    }
+
+    public PersonaModel(int id, String nombre, String apellido, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
