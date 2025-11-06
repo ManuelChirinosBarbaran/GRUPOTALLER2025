5c563cdb63c672eade0ce4b23edab5a9cd282811
Manuel <marcoschb27@gmail.com>
2025-11-06 10:04:42 -0500

    creacion de proyecto

---
+++ Taller-2025/src/main/java/com/example/Taller_2025/Taller2025Application.java
@@ -0,0 +1,13 @@
+package com.example.Taller_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller2025Application.class, args);
+	}
+
+}
---
+++ Taller-2025/src/test/java/com/example/Taller_2025/Taller2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Taller_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
